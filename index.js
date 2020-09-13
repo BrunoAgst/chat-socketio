a1aa0afd892c4108a0d8f8efb5f4ad4d6c880d9c
BrunoAgst <bruno_agst@hotmail.com>
2020-09-12 23:42:37 -0300

    projeto feito no curso formação nodejs

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const http = require("http").createServer(app);
+const io =  require('socket.io')(http);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+
+    socket.on("disconnect", () => {
+        console.log("desconectou" + socket.id);
+    });
+
+    socket.on("msg", (data) => {
+        io.emit("showmsg", data);
+    });
+
+});
+
+http.listen(3000, () => {
+    console.log("app rodando");
+});
